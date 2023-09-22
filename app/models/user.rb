707ea2efad0a27700038e116ac9bbdabdd7124a7
Chuck Eiker <chuckeiker@gmail.com>
2023-09-22 11:06:18 -0400

    Save local changes in user.rb and sessions_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :sessions
 
+  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+  before_create :hash_password
+  private
+  def hash_password
+    self.password = BCrypt::Password.create(self.password)
+  end
+
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 } 
   validates_uniqueness_of :username
